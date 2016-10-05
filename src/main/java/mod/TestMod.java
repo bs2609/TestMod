eca8c2723e82d39c0eb4ea8f93798aa5cbd51617
Ben Staddon <quadraxis@hotmail.co.uk>
2016-10-05 21:30:08 +0100

    Update mod version

--- src/main/java/mod/TestMod.java
+++ src/main/java/mod/TestMod.java
@@ -18,7 +18,7 @@ public class TestMod {
 
 	public static final String MOD_ID = "testing";
 	public static final String MOD_NAME = "a test mod";
-	public static final String VERSION = "0.0.7";
+	public static final String VERSION = "0.1.0";
 
 	@Mod.Instance(MOD_ID)
 	public static TestMod instance;
@@ -34,7 +34,6 @@ public class TestMod {
 	public void preInit(FMLPreInitializationEvent event) {
 		logger = event.getModLog();
 		config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
 		proxy.preInit(event);
 	}
 
