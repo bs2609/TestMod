bbbe35bada8067a260ee2e26219e17bb7119630a
Ben Staddon <quadraxis@hotmail.co.uk>
2016-10-25 21:42:48 +0100

    Add another command for testing things

---
+++ src/main/java/mod/command/CommandSample.java
@@ -0,0 +1,47 @@
+package mod.command;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.chunk.Chunk;
+
+public class CommandSample extends CommandBase {
+	
+	@Override
+	public String getCommandName() {
+		return "sample";
+	}
+	
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "";
+	}
+	
+	@Override
+	public int getRequiredPermissionLevel() {
+		return 2;
+	}
+	
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		
+		Chunk chunk = sender.getEntityWorld().getChunkFromBlockCoords(sender.getPosition());
+		Multiset<IBlockState> states = HashMultiset.create();
+		
+		for (int x = 0; x < 16; ++x) {
+			for (int z = 0; z < 16; ++z) {
+				for (int y = 0; y < 256; ++y) {
+					states.add(chunk.getBlockState(x, y, z));
+				}
+			}
+		}
+		
+		String msg = "Chunk [" + chunk.xPosition + ", " + chunk.zPosition + "] contains " + states.elementSet().size() + " blockstates";
+		sender.addChatMessage(new TextComponentString(msg));
+	}
+}
--- src/main/java/mod/command/ModCommands.java
+++ src/main/java/mod/command/ModCommands.java
@@ -3,6 +3,7 @@ package mod.command;
 import net.minecraft.command.ICommand;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ModCommands {
@@ -10,10 +11,11 @@ public class ModCommands {
 	private static List<ICommand> commands = new ArrayList<ICommand>();
 	
 	static {
+		commands.add(new CommandSample());
 		commands.add(new CommandSurreal());
 	}
 	
-	public static List<ICommand> getCommands() {
+	public static Collection<ICommand> getCommands() {
 		return commands;
 	}
 }
