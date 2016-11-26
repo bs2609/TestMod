ee45f6bee14fcb4d17059ed9167c5bee85fc6190
Ben Staddon <quadraxis@hotmail.co.uk>
2016-11-26 05:16:33 +0000

    Bump version

--- src/main/java/mod/TestMod.java
+++ src/main/java/mod/TestMod.java
@@ -25,7 +25,7 @@ public class TestMod {
 
 	public static final String MOD_ID = "testing";
 	public static final String MOD_NAME = "a test mod";
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "0.2.5";
 
 	@Mod.Instance(MOD_ID)
 	public static TestMod instance;
