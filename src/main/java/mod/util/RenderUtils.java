53779567bdb0c07481463b246ad24b09765ac002
Ben Staddon <quadraxis@hotmail.co.uk>
2016-12-07 00:11:25 +0000

    Add access to vanilla cloud renderer

--- src/main/java/mod/util/RenderUtils.java
+++ src/main/java/mod/util/RenderUtils.java
@@ -1,5 +1,6 @@
 package mod.util;
 
+import mod.TestMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.RenderGlobal;
@@ -9,6 +10,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 @SideOnly(Side.CLIENT)
 public class RenderUtils {
@@ -22,8 +24,24 @@ public class RenderUtils {
 		public void render(float partialTicks, WorldClient world, Minecraft mc) {
 			try {
 				renderMethod.invoke(mc.renderGlobal);
-			} catch (Exception e) {}
+			} catch (Exception e) {
+				TestMod.getLogger().warn("Exception invoking " + Arrays.toString(methodNames), e);
+			}
 		}
 	}
 	
+	public static class CloudRenderer extends IRenderHandler {
+		
+		private static final String[] methodNames = {"renderCloudsFancy", "func_180445_c"};
+		private static final Method renderMethod = ReflectionHelper.findMethod(RenderGlobal.class, null, methodNames, float.class, int.class);
+		
+		@Override
+		public void render(float partialTicks, WorldClient world, Minecraft mc) {
+			try {
+				renderMethod.invoke(mc.renderGlobal, partialTicks, 2);
+			} catch (Exception e) {
+				TestMod.getLogger().warn("Exception invoking " + Arrays.toString(methodNames), e);
+			}
+		}
+	}
 }
