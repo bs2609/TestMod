ed441b1f93f29e2f2592f9b8e36e52db6fc3d211
Ben Staddon <quadraxis@hotmail.co.uk>
2017-03-16 09:20:41 +0000

    Continue tidying parameters

--- src/main/java/mod/world/AbstractWorldEventListener.java
+++ src/main/java/mod/world/AbstractWorldEventListener.java
@@ -11,25 +11,25 @@ import net.minecraft.world.World;
 
 public abstract class AbstractWorldEventListener implements IWorldEventListener {
 	
-	public void notifyBlockUpdate(World worldIn, BlockPos pos, IBlockState oldState, IBlockState newState, int flags) {}
+	public void notifyBlockUpdate(World world, BlockPos pos, IBlockState oldState, IBlockState newState, int flags) {}
 	
 	public void notifyLightSet(BlockPos pos) {}
 	
 	public void markBlockRangeForRenderUpdate(int x1, int y1, int z1, int x2, int y2, int z2) {}
 	
-	public void playSoundToAllNearExcept(EntityPlayer player, SoundEvent soundIn, SoundCategory category, double x, double y, double z, float volume, float pitch) {}
+	public void playSoundToAllNearExcept(EntityPlayer player, SoundEvent sound, SoundCategory category, double x, double y, double z, float volume, float pitch) {}
 	
-	public void playRecord(SoundEvent soundIn, BlockPos pos) {}
+	public void playRecord(SoundEvent sound, BlockPos pos) {}
 	
 	public void spawnParticle(int particleID, boolean ignoreRange, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed, double zSpeed, int... parameters) {}
 	
-	public void onEntityAdded(Entity entityIn) {}
+	public void onEntityAdded(Entity entity) {}
 	
-	public void onEntityRemoved(Entity entityIn) {}
+	public void onEntityRemoved(Entity entity) {}
 	
 	public void broadcastSound(int soundID, BlockPos pos, int data) {}
 	
-	public void playEvent(EntityPlayer player, int type, BlockPos blockPosIn, int data) {}
+	public void playEvent(EntityPlayer player, int type, BlockPos pos, int data) {}
 	
 	public void sendBlockBreakProgress(int breakerId, BlockPos pos, int progress) {}
 	
