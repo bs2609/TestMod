cf7c464ba79a88b2892fe7932c843aee26858649
Ben Staddon <quadraxis@hotmail.co.uk>
2017-04-11 18:48:35 +0100

    Fixup ore dictionary matcher

--- src/main/java/mod/util/OreDictMatcher.java
+++ src/main/java/mod/util/OreDictMatcher.java
@@ -6,21 +6,24 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class OreDictMatcher implements Predicate<IBlockState> {
 	
-	private final Set<Block> blocks = new HashSet<Block>();
+	private final List<ItemStack> stacks;
 	
 	public OreDictMatcher(String name) {
-		for (ItemStack stack : OreDictionary.getOres(name, false)) {
-			blocks.add(Block.getBlockFromItem(stack.getItem()));
-		}
+		stacks = OreDictionary.getOres(name, false);
 	}
 	
 	@Override
 	public boolean apply(IBlockState input) {
-		return input != null && blocks.contains(input.getBlock());
+		Block block = input.getBlock();
+		for (ItemStack stack : stacks) {
+			if (block != Block.getBlockFromItem(stack.getItem())) continue;
+			int meta = stack.getMetadata();
+			if (meta == OreDictionary.WILDCARD_VALUE || meta == block.damageDropped(input)) return true;
+		}
+		return false;
 	}
 }
