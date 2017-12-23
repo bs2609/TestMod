282fe77541e83190f1262d67e77c222cc3808f23
Ben Staddon <quadraxis@hotmail.co.uk>
2017-12-23 07:38:30 +0000

    Bump versions

--- src/main/java/mod/TestMod.java
+++ src/main/java/mod/TestMod.java
@@ -16,13 +16,14 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.Logger;
 
+@SuppressWarnings("unused")
 @Mod(modid = TestMod.MOD_ID, name = TestMod.NAME, version = TestMod.VERSION,
-		dependencies = "required-after:forge@[14.23.0.2526,)", acceptedMinecraftVersions = "[1.12.2,1.13)")
+		dependencies = "required-after:forge@[14.23.1.2571,)", acceptedMinecraftVersions = "[1.12.2,1.13)")
 public class TestMod {
 
 	public static final String MOD_ID = "testing";
 	public static final String NAME = "a test mod";
-	public static final String VERSION = "0.4.1";
+	public static final String VERSION = "0.4.2";
 
 	@Mod.Instance(MOD_ID)
 	public static TestMod instance;
